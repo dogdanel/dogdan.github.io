a3da13461115bafea7c1b2abd6eddba51e789f6b
dogdanel <144874543+dogdanel@users.noreply.github.com>
2023-10-04 10:50:15 +0300

    My first site

---
+++ index.js
@@ -0,0 +1,15 @@
+var nume=document.querySelector(".name")
+var oc=document.querySelector(".ocupation")
+
+
+
+setTimeout(function()
+{
+    nume.style.opacity=1;
+nume.style.transform="translateX(200px)"
+},1000)
+setTimeout(function()
+{
+    oc.style.opacity=1;
+    oc.style.transform="translateX(100px)"
+},2000)
